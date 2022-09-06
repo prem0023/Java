467d93876ded3d06ed0835122c687ec7c0a03232
prem023 <47606671+prem0023@users.noreply.github.com>
2022-09-06 14:58:43 +0530

    Update Next_Right_Greater_Element.java
    added better implementation

--- Next_Right_Greater_Element.java
+++ Next_Right_Greater_Element.java
@@ -1,5 +1,41 @@
 //Next Right Greater Element
 
+// Method 1 --> Time O(n), Space O(n)
+class Solution
+{
+    
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        Stack<Long> st = new Stack<Long>();
+        //int[] ans = new int[n];
+        long a = 0;
+        for(int i=n-1;i>=0;i--){
+            a = arr[i];
+            if(st.isEmpty())
+                arr[i] = -1;
+                
+            else{
+                while(!st.isEmpty()){
+                    if(a < st.peek()){
+                        arr[i] = st.peek();
+                        break;
+                    }
+                    else
+                        st.pop();
+                    arr[i] = -1;
+                }
+            }
+            st.push(a);
+            //j--;
+        }
+        return arr;
+    } 
+}
+
+/*
+Method 2 --> Time O(n) space O(2*n)
 class Solution
 {
     //Function to find the next greater element for each element of the array.
@@ -29,3 +65,4 @@ class Solution
         return l;
     } 
 }
+*/
