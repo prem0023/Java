48b59926ec325402710d552ba4a756eb44c8c4e7
prem023 <47606671+prem0023@users.noreply.github.com>
2022-09-09 13:29:52 +0530

    Update Max_Area_Rectangle_in_Binary_Matrix.java
    added new approach

--- Max_Area_Rectangle_in_Binary_Matrix.java
+++ Max_Area_Rectangle_in_Binary_Matrix.java
@@ -58,3 +58,61 @@ class Solution {
         return max; 
     }
 }
+
+/*
+// Method 2 DP approach
+
+class Solution {
+	int max = 0;
+
+	public int maximalRectangle(char[][] matrix) {
+		if (matrix.length == 0)
+			return 0;
+		int m = matrix.length;
+		int n = matrix[0].length;
+
+		int[] left = new int[n];
+		int[] right = new int[n];
+		int[] height = new int[n];
+
+		int maxA = 0;
+        
+        for (int i = 0; i < n; i++){
+            right[i] = n;
+        } 
+
+		for (int i = 0; i < m; i++) {
+			int cur_left = 0, cur_right = n;
+			for (int j = 0; j < n; j++) { // compute height (can do this from either side)
+				if (matrix[i][j] == '1')
+					height[j]++;
+				else
+					height[j] = 0;
+			}
+			for (int j = 0; j < n; j++) { // compute left (from left to right)
+				if (matrix[i][j] == '1')
+					left[j] = Math.max(left[j], cur_left);
+				else {
+					left[j] = 0;
+					cur_left = j + 1;
+				}
+			}
+			// compute right (from right to left)
+			for (int j = n - 1; j >= 0; j--) {
+                //right[j] = n;
+				if (matrix[i][j] == '1')
+					right[j] = Math.min(right[j], cur_right);
+				else {
+					right[j] = n;
+					cur_right = j;
+				}
+			}
+			// compute the area of rectangle (can do this from either side)
+			for (int j = 0; j < n; j++)
+				maxA = Math.max(maxA, (right[j] - left[j]) * height[j]);
+		}
+		return maxA;
+	}
+}
+
+*/
