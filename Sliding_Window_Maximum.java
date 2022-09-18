d983142f2b6ce4ff2b150ff2616973fc5e598b71
prem023 <47606671+prem0023@users.noreply.github.com>
2022-09-18 18:54:57 +0530

    Add files via upload

---
+++ Sliding_Window_Maximum.java
@@ -0,0 +1,84 @@
+// Sliding Window Maximum
+
+https://leetcode.com/problems/sliding-window-maximum/
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+			return new int[0];
+		}
+        int[] rt = new int[nums.length-k+1];
+        int index = 0;
+        int indexLeft = 0;
+        int indexRight = k-1;
+        int indexMax = findIMax(nums, indexLeft, indexLeft + k);
+        rt[index++] = nums[indexMax];
+        indexLeft ++;
+        indexRight ++;
+        
+        while(indexRight < nums.length){
+            if(nums[indexRight] >= nums[indexMax]){
+                indexMax = indexRight;
+            } 
+            else if(indexLeft > indexMax){
+                // trouble. max is not in range
+                if (nums[indexRight] >= nums[indexMax]-1) {
+                    // if right is one less than maximum
+                    indexMax = indexRight;
+                }
+                else if (nums[indexLeft] >= nums[indexMax]-1) {
+                    // if left is one less than maximum
+                    indexMax = indexLeft;
+                }
+                else{
+                    indexMax = findIMax(nums, indexLeft, indexRight+1);
+                }
+            }
+            rt[index++] = nums[indexMax];
+            indexLeft++;
+            indexRight++;
+        }
+       
+        return rt;
+    }
+    private static int findIMax(int[] nums, int start, int end) {
+        int rt = start;
+        for (int i = start + 1; i < end; i++) {
+            if (nums[i] >= nums[rt]) {// biggest and rightmost
+                rt = i;
+            }
+        }
+        return rt;
+    }
+}
+
+/*
+// Method - 2
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        if (nums == null || k <= 0) {
+			return new int[0];
+		}
+        
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        int j = 0;
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+        
+        for(int i=0; i<n; i++){
+            if(!dq.isEmpty() && dq.peek() == i-k)
+                dq.poll();
+            
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
+                dq.pollLast();
+            dq.offer(i);
+            
+            if(!dq.isEmpty() && i >= k-1)
+                ans[j++] = nums[dq.peek()];
+                
+        }
+        return ans;
+    }
+}
+*/
