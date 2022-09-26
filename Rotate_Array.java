a8afd0b8cec1e060fff98510f4d09bdec5f00b22
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-09-26 21:23:38 +0530

    Add files via upload

--- Rotate_Array.java
+++ Rotate_Array.java
@@ -1,52 +1,46 @@
-package p1;
+// Rotate Array by d
 
-import java.util.Arrays;
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+         int m=-1;
 
-class Rotate_Array {
-	
-	public static void main(String[] args) {
-		int[] nums = {8,6,2,1,3};
-		//System.out.println(n);
-		rotate(nums,1);
-		//int[] b = {6,2,1,3};
-		System.out.println(Arrays.toString(nums));
-		//System.out.println(Arrays.toString(b));
-	}
-	
-	public static void rotate(int[] nums, int k) {
-		 int n = nums.length;
-	        int c=0;
-	        int[] a = new int[n];
-	        //System.out.print("[");
-	       for(int i=0;i<n;i++){
-	            if(n-k+i < n){
-	                a[c++] = nums[n-k+i];             
-	            }
-	            else{
-	                a[c++] = nums[i-k];        
-	            }
-	        }
-	        for (int i=0;i<n;i++){
-	            nums[i] = a[i];
-	        }
-	}
-	
-	
-    public static void rotate_new(int[] nums, int k) {
-        int n = nums.length;
-        //int c=0;
-        int i=0;
-        System.out.print("[");
-        for(i=0;i<n-1;i++){
-            if(n-k+i < n){
-               System.out.print(nums[n-k+i]+",");
-                   
-            }
-            else{
-                System.out.print(nums[i-k]+ ",");
-            }
-            
+        d=d%n;
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            m=Math.max(m,arr[i]);
+
         }
-        System.out.println(nums[i-k]+"]");
+
+        m++;
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            if(i>=d)
+
+                arr[(i-d)]+=(arr[i]%m)*m;
+
+            else
+
+                arr[(n+i-d)]+=(arr[i]%m)*m;
+
+        }
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            arr[i]=arr[i]/m;
+
+        }
+
     }
 }
