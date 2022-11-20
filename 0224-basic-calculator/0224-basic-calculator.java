78ba7da8d707734aa3a7710d44c83a07a835eee5
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-11-20 12:38:23 +0530

    Time: 13 ms (76.91%), Space: 43.8 MB (79.79%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,29 +1,37 @@
 class Solution {
-    public static int calculate(String s) {
-	int len = s.length(), sign = 1, result = 0;
-	Stack<Integer> stack = new Stack<Integer>();
-	for (int i = 0; i < len; i++) {
-		if (Character.isDigit(s.charAt(i))) {
-			int sum = s.charAt(i) - '0';
-			while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
-				sum = sum * 10 + s.charAt(i + 1) - '0';
-				i++;
-			}
-			result += sum * sign;
-		} else if (s.charAt(i) == '+')
-			sign = 1;
-		else if (s.charAt(i) == '-')
-			sign = -1;
-		else if (s.charAt(i) == '(') {
-			stack.push(result);
-			stack.push(sign);
-			result = 0;
-			sign = 1;
-		} else if (s.charAt(i) == ')') {
-			result = result * stack.pop() + stack.pop();
-		}
-
+    public int calculate(String s) {
+    Stack<Integer> stack = new Stack<Integer>();
+    int result = 0;
+    int number = 0;
+    int sign = 1;
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        if(Character.isDigit(c)){
+            number = 10 * number + (int)(c - '0');
+        }else if(c == '+'){
+            result += sign * number;
+            number = 0;
+            sign = 1;
+        }else if(c == '-'){
+            result += sign * number;
+            number = 0;
+            sign = -1;
+        }else if(c == '('){
+            //we push the result first, then sign;
+            stack.push(result);
+            stack.push(sign);
+            //reset the sign and result for the value in the parenthesis
+            sign = 1;   
+            result = 0;
+        }else if(c == ')'){
+            result += sign * number;  
+            number = 0;
+            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+            
+        }
     }
-	return result;
-    }
+    if(number != 0) result += sign * number;
+    return result;
+}
 }
