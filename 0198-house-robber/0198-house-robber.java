2190ca74f4b45937cec23039629b0efa2824ea1a
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-11-21 04:04:17 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (7.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] num) {
+        int prevNo = 0;
+        int prevYes = 0;
+        for (int n : num) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = n + temp;
+        }
+        return Math.max(prevNo, prevYes);
+    }
+}
+
+/*
+public int rob(int[] num) {
+    int[][] dp = new int[num.length + 1][2];
+    for (int i = 1; i <= num.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        dp[i][1] = num[i - 1] + dp[i - 1][0];
+    }
+    return Math.max(dp[num.length][0], dp[num.length][1]);
+}
+
+*/
