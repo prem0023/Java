a17832113983bc772ea92e09218907681040e5fa
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-11-27 13:55:02 +0530

    Time: 319 ms (56.71%), Space: 112.3 MB (69.51%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -1,23 +1,4 @@
 class Solution {
-    public int numberOfArithmeticSlices(int[] nums) {
-        int n = nums.length;
-        int ans = 0;
-        HashMap<Long, Integer>[] dp = new HashMap[n];
-        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                long diff = (long) nums[i] - (long) nums[j];
-                int cnt = dp[j].getOrDefault(diff, 0);
-                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + cnt + 1);
-                ans += cnt;
-            }
-        }
-        return ans;
-    }
-}
-
-/*
-class Solution {
     public int numberOfArithmeticSlices(int[] A) {
         int res = 0;
         Map<Integer, Integer>[] map = new Map[A.length];
@@ -40,4 +21,25 @@ class Solution {
         return res;
     }
 }
+
+/*
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        HashMap<Long, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - (long) nums[j];
+                int cnt = dp[j].getOrDefault(diff, 0);
+                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + cnt + 1);
+                ans += cnt;
+            }
+        }
+        return ans;
+    }
+}
+
 */
