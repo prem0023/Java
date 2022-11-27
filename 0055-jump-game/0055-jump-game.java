e23415220e37402979e382efbd05b78e31326a2f
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-11-27 14:00:09 +0530

    Time: 2 ms (94.60%), Space: 42.8 MB (91.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,15 @@
-// class Solution {
-//     public boolean canJump(int[] nums) {
-//         int reachable = 0;
-//         for (int i=0; i<nums.length; ++i) {
-//             if (i > reachable) return false;
-//             reachable = Math.max(reachable, i + nums[i]);
-//         }
-//         return true;
-//     }
-// }
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+        for (int i=0; i<nums.length; ++i) {
+            if (i > reachable) return false;
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+}
 
+/*
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
@@ -21,3 +22,4 @@ class Solution {
         return true;
     }
 }
+*/
