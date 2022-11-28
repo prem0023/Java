21f93880512f9df90d921c3982962b2d17c09950
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-11-29 02:41:23 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (57.74%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,6 +13,20 @@
  *     }
  * }
  */
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
+
+/*
 class Solution {
     public boolean isValidBST(TreeNode root) {
        if (root == null) return true;
@@ -31,3 +45,4 @@ class Solution {
        return true;
     }
 }
+*/
