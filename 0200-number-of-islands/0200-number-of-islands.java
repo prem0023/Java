3fa5e54109815bf87ca95d734433753bb12cc228
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-15 00:19:53 +0530

    Time: 5 ms (76.88%), Space: 49.5 MB (99.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,55 @@
-public class Solution {
+class Pair{
+    int i, j;
+    Pair(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+}
 
-    private int n;
-    private int m;
-
+class Solution {
     public int numIslands(char[][] grid) {
+        
         int count = 0;
-        n = grid.length;
-        if (n == 0) return 0;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    count++;
+                    visited[i][j] = true;
+                    DFS(visited, grid, i, j, n, m);
                 }
-        }    
+            }
+        }
         return count;
     }
-
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+    void DFS(boolean[][] visited, char[][] grid, int i, int j, int n, int m){
+        
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(i, j));
+        
+        while(!queue.isEmpty()){
+            int x = queue.peek().i;
+            int y = queue.poll().j;
+            
+            if(x>0 && grid[x-1][y] == '1' && !visited[x-1][y]){
+                visited[x-1][y] = true;
+                queue.offer(new Pair(x-1, y));
+            }
+            if(y>0 && grid[x][y-1] == '1' && !visited[x][y-1]){
+                visited[x][y-1] = true;
+                queue.offer(new Pair(x, y-1));
+            }
+            if(x+1<n && grid[x+1][y] == '1' && !visited[x+1][y]){
+                visited[x+1][y] = true;
+                queue.offer(new Pair(x+1, y));
+            }
+            if(y+1<m && grid[x][y+1] == '1' && !visited[x][y+1]){
+                visited[x][y+1] = true;
+                queue.offer(new Pair(x, y+1));
+            }
+        }
     }
 }
