e383ef5ead8562b0ec56138c669721cceb750ea3
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-15 01:53:59 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (97.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,3 +1,21 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] != newColor)
+            dfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    private void dfs(int[][] image, int i, int j, int c0, int c1) {
+        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != c0) return;
+        image[i][j] = c1;
+        dfs(image, i, j - 1, c0, c1);
+        dfs(image, i, j + 1, c0, c1);
+        dfs(image, i - 1, j, c0, c1);
+        dfs(image, i + 1, j, c0, c1);
+    }
+}
+
+/*
 class Pair{
     int i, j;
     Pair(int i, int j){
@@ -47,3 +65,4 @@ class Solution {
         return image;
     }
 }
+*/
