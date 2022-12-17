89c836094058961c89d935e861581df17cc9caca
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-17 15:20:57 +0530

    Time: 5 ms (96.91%), Space: 42.3 MB (80.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,24 @@
-public class Solution {
+class Solution {
     public int evalRPN(String[] tokens) {
-        int a,b;
-		Stack<Integer> S = new Stack<Integer>();
-		for (String s : tokens) {
-			if(s.equals("+")) {
-				S.add(S.pop()+S.pop());
-			}
-			else if(s.equals("/")) {
-				b = S.pop();
-				a = S.pop();
-				S.add(a / b);
-			}
-			else if(s.equals("*")) {
-				S.add(S.pop() * S.pop());
-			}
-			else if(s.equals("-")) {
-				b = S.pop();
-				a = S.pop();
-				S.add(a - b);
-			}
-			else {
-				S.add(Integer.parseInt(s));
-			}
-		}	
-		return S.pop();
-	}
+        Stack<Integer> stack = new Stack<>() ;
+        Set<String> o = new HashSet<>(Arrays.asList("+", "-", "*", "/")) ;
+        for (String i:tokens) 
+        {
+            if(!o.contains(i)) 
+                stack.push(Integer.valueOf(i));
+            else 
+            {
+                int a = stack.pop(), b = stack.pop();
+                if(i.equals("+")) 
+                    stack.push(a + b);
+                else if(i.equals("-")) 
+                    stack.push(b - a);
+                else if(i.equals("*")) 
+                    stack.push(a*b);
+                else 
+                    stack.push(b/a);
+            }
+        }
+        return stack.peek() ;
+    }
 }
