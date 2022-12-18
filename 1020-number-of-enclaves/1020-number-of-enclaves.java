cd8549583e509902a2ba7f104668ccd0c7c13aa0
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-18 22:45:41 +0530

    Time: 20 ms (41.01%), Space: 77.1 MB (23.64%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,52 @@
+class Pair{
+    int x;
+    int y;
+    Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Solution {
+    public int numEnclaves(int[][] a) {
+        int n = a.length;
+        int m = a[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int ans = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(a[i][j] == 1 && !visited[i][j]){
+                        DFS(a, visited, i, j, n, m);
+                    }
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j] && a[i][j] == 1)
+                    ans++;
+            }
+        }
+        
+        return ans;
+    }
+    static void DFS(int a[][], boolean[][] visited, int x, int y, int n, int m) {
+		visited[x][y] = true;
+		
+		if (x > 0 && a[x - 1][y] == 1 && !visited[x - 1][y])
+			DFS(a, visited, x - 1, y, n, m);
+			
+		if (x + 1 < n && a[x + 1][y] == 1 && !visited[x + 1][y])
+			DFS(a, visited, x + 1, y, n, m);
+			
+		if (y > 0 && a[x][y - 1] == 1 && !visited[x][y - 1])
+			DFS(a, visited, x, y - 1, n, m);
+			
+		if (y + 1 < m && a[x][y + 1] == 1 && !visited[x][y + 1])
+			DFS(a, visited, x, y + 1, n, m);
+			
+	}
+}
