02b8741923dfdf3d2681bf95a5925c854157c75f
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-18 20:31:40 +0530

    Time: 1 ms (99.88%), Space: 44.9 MB (79.39%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,48 +1,45 @@
 class Solution {
-    public void solve(char[][] board) {
-        if (board.length == 0 || board[0].length == 0)
-            return;
-        if (board.length < 2 || board[0].length < 2)
-            return;
-        int m = board.length, n = board[0].length;
-        //Any 'O' connected to a boundary can't be turned to 'X', so ...
-        //Start from first and last column, turn 'O' to '*'.
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O')
-                boundaryDFS(board, i, 0);
-            if (board[i][n-1] == 'O')
-                boundaryDFS(board, i, n-1);	
-        }
-        //Start from first and last row, turn '0' to '*'
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O')
-                boundaryDFS(board, 0, j);
-            if (board[m-1][j] == 'O')
-                boundaryDFS(board, m-1, j);	
-        }
-        //post-prcessing, turn 'O' to 'X', '*' back to 'O', keep 'X' intact.
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O')
-                    board[i][j] = 'X';
-                else if (board[i][j] == '*')
-                    board[i][j] = 'O';
+    public void solve(char[][] a) {
+        
+        int n = a.length;
+        int m = a[0].length;
+        boolean[][] visited = new boolean[n][m];
+        char[][] ans = new char[n][m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(a[i][j] == 'O' && !visited[i][j]){
+                        DFS(a, ans, visited, i, j, n, m);
+                    }
+                }
             }
         }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!visited[i][j])
+                    a[i][j] = 'X';
+                else
+                    a[i][j] = 'O';
+            }
+        }
+
     }
-    //Use DFS algo to turn internal however boundary-connected 'O' to '*';
-    private void boundaryDFS(char[][] board, int i, int j) {
-        if (i < 0 || i > board.length - 1 || j <0 || j > board[0].length - 1)
-            return;
-        if (board[i][j] == 'O')
-            board[i][j] = '*';
-        if (i > 1 && board[i-1][j] == 'O')
-            boundaryDFS(board, i-1, j);
-        if (i < board.length - 2 && board[i+1][j] == 'O')
-            boundaryDFS(board, i+1, j);
-        if (j > 1 && board[i][j-1] == 'O')
-            boundaryDFS(board, i, j-1);
-        if (j < board[i].length - 2 && board[i][j+1] == 'O' )
-            boundaryDFS(board, i, j+1);
-    }
+    static void DFS(char a[][], char[][] ans, boolean[][] visited, int x, int y, int n, int m) {
+		visited[x][y] = true;
+		
+		if (x > 0 && a[x - 1][y] == 'O' && !visited[x - 1][y])
+			DFS(a, ans, visited, x - 1, y, n, m);
+			
+		if (x + 1 < n && a[x + 1][y] == 'O' && !visited[x + 1][y])
+			DFS(a, ans, visited, x + 1, y, n, m);
+			
+		if (y > 0 && a[x][y - 1] == 'O' && !visited[x][y - 1])
+			DFS(a, ans, visited, x, y - 1, n, m);
+			
+		if (y + 1 < m && a[x][y + 1] == 'O' && !visited[x][y + 1])
+			DFS(a, ans, visited, x, y + 1, n, m);
+			
+	}
 }
