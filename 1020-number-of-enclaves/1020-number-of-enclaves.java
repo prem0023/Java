d8d6678b50a129d1d5a9a4311bcee8c8b068cde1
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-18 23:05:51 +0530

    Time: 9 ms (92.07%), Space: 55.3 MB (85.43%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -6,14 +6,26 @@ class Solution {
         int ans = 0;
         
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i == 0 || j == 0 || i == n-1 || j == m-1){
-                    if(a[i][j] == 1 && !visited[i][j]){
-                        DFS(a, visited, i, j, n, m);
-                    }
-                }
-            }
+            if(!visited[i][0] && a[i][0] == 1)
+                DFS(a, visited, i, 0, n, m);
+            if(!visited[i][m-1] && a[i][m-1] == 1)
+                DFS(a, visited, i, m-1, n, m);
         }
+        for(int j=0; j<m; j++){
+            if(!visited[0][j] && a[0][j] == 1)
+                DFS(a, visited, 0, j, n, m);
+            if(!visited[n-1][j] && a[n-1][j] == 1)
+                DFS(a, visited, n-1, j, n, m);
+        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(i == 0 || j == 0 || i == n-1 || j == m-1){
+        //             if(a[i][j] == 1 && !visited[i][j]){
+        //                 DFS(a, visited, i, j, n, m);
+        //             }
+        //         }
+        //     }
+        // }
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
