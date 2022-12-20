0fa4ea9344de9b12ea67dec6d24f14f444754e15
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-20 22:15:25 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -64,31 +64,32 @@ class Solution
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
         // add your code here
-        int[] ans = new int[V];
-        boolean[] visited = new boolean[V];
-        Stack<Integer> stack = new Stack<>();
+        int[] indegree = new int[V];
+        Queue<Integer> queue = new LinkedList<>();
+        int[] topo = new int[V];
         
         for(int i=0; i<V; i++){
-            if(!visited[i])
-                DFS(adj, stack, visited, i);
+            for(int x : adj.get(i))
+                indegree[x]++;
         }
+        
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0)
+                queue.offer(i);
+        }
+        
         int k = 0;
-        
-        while(!stack.isEmpty())
-            ans[k++] = stack.pop();
-        return ans;
-    }
-    
-    private static void DFS(ArrayList<ArrayList<Integer>> adj, Stack<Integer> stack, boolean[] visited, int i){
-        
-        visited[i] = true;
-        
-        for(int x : adj.get(i)){
-            if(!visited[x]){
-                DFS(adj, stack, visited, x);
-                visited[x] = true;
+        while(!queue.isEmpty()){
+            int i = queue.poll();
+            if(indegree[i] == 0){
+                topo[k++] = i;
+                for(int x : adj.get(i)){
+                    indegree[x]--;
+                    if(indegree[x] == 0)
+                        queue.offer(x);
+                }
             }
         }
-        stack.push(i);
+        return topo;
     }
 }
