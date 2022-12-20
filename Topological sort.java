fa1cc78c8b93d53cf6c1e143cd9140782e6ef1fd
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2022-12-20 22:17:03 +0530

    Update Topological sort.java

--- Topological sort.java
+++ Topological sort.java
@@ -1,3 +1,44 @@
+// kahn's Algo
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int[] indegree = new int[V];
+        Queue<Integer> queue = new LinkedList<>();
+        int[] topo = new int[V];
+        
+        for(int i=0; i<V; i++){
+            for(int x : adj.get(i))
+                indegree[x]++;
+        }
+        
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0)
+                queue.offer(i);
+        }
+        
+        int k = 0;
+        while(!queue.isEmpty()){
+            int i = queue.poll();
+            if(indegree[i] == 0){
+                topo[k++] = i;
+                for(int x : adj.get(i)){
+                    indegree[x]--;
+                    if(indegree[x] == 0)
+                        queue.offer(x);
+                }
+            }
+        }
+        return topo;
+    }
+}
+
+
+
+/*
+//DFS Algo
 class Solution
 {
     //Function to return list containing vertices in Topological order. 
@@ -32,3 +73,4 @@ class Solution
         stack.push(i);
     }
 }
+*/
