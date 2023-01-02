d2d599887c96f62faee7eb0ed2ca4a9eb89006e4
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-02 23:46:58 +0530

    Added solution - LeetHub

---
+++ Connecting the graph - GFG/connecting-the-graph.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] edge = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                edge[i][0] = sc.nextInt();
+                edge[i][1] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            int ans = obj.Solve(n, edge);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    public int Solve(int n, int[][] edge) {
+        // Code here
+        DisjointSet dj = new DisjointSet(n);
+        int m = edge.length;
+        int extraEdges = 0;
+        int provinces = 0;
+        
+        for(int i=0; i<m; i++){
+            if(dj.findUPar(edge[i][0]) != dj.findUPar(edge[i][1]))
+                dj.unionByRank(edge[i][0], edge[i][1]);
+            else
+                extraEdges++;
+        }
+        
+        for(int i=0; i<n; i++){
+            if(dj.findUPar(i) == i)
+                provinces++;
+        }
+        provinces--;
+        
+        if(provinces <= extraEdges)
+            return provinces;
+        
+        return -1;
+    }
+}
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>(); 
+    public DisjointSet(int n) {
+        for(int i = 0;i<=n;i++) {
+            rank.add(0); 
+            parent.add(i); 
+            size.add(1); 
+        }
+    }
+
+    public int findUPar(int node) {
+        if(node == parent.get(node)) {
+            return node; 
+        }
+        int ulp = findUPar(parent.get(node)); 
+        parent.set(node, ulp); 
+        return parent.get(node); 
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+        }
+        else if(rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            int rankU = rank.get(ulp_u); 
+            rank.set(ulp_u, rankU + 1); 
+        }
+    }
+    
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
