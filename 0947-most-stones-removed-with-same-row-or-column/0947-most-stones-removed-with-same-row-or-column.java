70a6747ca4f61412bfcd605e193c4475352a5f65
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-06 18:01:09 +0530

    Time: 46 ms (63.45%), Space: 54.8 MB (13.70%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,23 +1,80 @@
 class Solution {
     public int removeStones(int[][] stones) {
-        boolean[] visited = new boolean[stones.length];
-        int islands = 0;
-        for (int i = 0; i < stones.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(stones[i], visited, stones);
-                islands++;
-            }
+        int n = stones.length;
+        int maxRow = 0;
+        int maxCol = 0;
+        int count = 0;
+        
+        for(int i=0; i<n; i++){
+            maxRow = Math.max(maxRow, stones[i][0]);
+            maxCol = Math.max(maxCol, stones[i][1]);
         }
-        return stones.length - islands;
+        
+        DisjointSet dj = new DisjointSet(maxRow+maxCol+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<n; i++){
+            map.put(stones[i][0], 1);
+            map.put(maxRow+stones[i][1]+1, 1);
+            dj.unionBySize(stones[i][0], maxRow+stones[i][1]+1);
+        }
+        for(Map.Entry<Integer, Integer> it : map.entrySet()){
+            if(dj.findUPar(it.getKey()) == it.getKey())
+                count++;
+        }
+        return n-count;
+    }
+};
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>(); 
+    public DisjointSet(int n) {
+        for(int i = 0;i<=n;i++) {
+            rank.add(0); 
+            parent.add(i); 
+            size.add(1); 
+        }
+    }
+
+    public int findUPar(int node) {
+        if(node == parent.get(node)) {
+            return node; 
+        }
+        int ulp = findUPar(parent.get(node)); 
+        parent.set(node, ulp); 
+        return parent.get(node); 
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+        }
+        else if(rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            int rankU = rank.get(ulp_u); 
+            rank.set(ulp_u, rankU + 1); 
+        }
     }
     
-    private void dfs(int[] stone, boolean[] visited, int[][] stones) {
-        for (int i = 0; i < stones.length; i++) {
-            if (!visited[i] && (stone[0] == stones[i][0] || stone[1] == stones[i][1])) {
-                visited[i] = true;
-                dfs(stones[i], visited, stones);
-            }
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
         }
     }
 }
