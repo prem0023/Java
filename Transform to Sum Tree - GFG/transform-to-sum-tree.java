5fcd570d4e509bd750e13c33eefa5f4082d73189
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-17 11:18:21 +0530

    Added solution - LeetHub

---
+++ Transform to Sum Tree - GFG/transform-to-sum-tree.java
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution ob = new Solution();
+        	    ob.toSumTree(root);
+        	    printInorder(root);
+        	    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/*Complete the Given Function
+
+Node is as follows:
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+    	data=d;
+    	left=null;
+    	right=null;
+    }
+}*/
+
+class Solution{
+    public void toSumTree(Node root){
+        if(root == null)
+	        return;
+	    HashMap<Node, Integer> map = new HashMap<>();
+	    Queue<Node> q = new LinkedList<>();
+	    
+	    findisSumTree(root, map);
+	    
+	    q.offer(root);
+	    while(!q.isEmpty()){
+	        Node temp = q.poll();
+	        temp.data = map.get(temp);
+	        
+	        if(temp.left != null)
+	            q.offer(temp.left);
+	        if(temp.right != null)
+	            q.offer(temp.right);
+	    }
+	}
+	
+	private void findisSumTree(Node root, HashMap<Node, Integer> map){
+	    if(root == null)
+            return;
+        
+        if(root.left == null && root.right == null){
+            map.put(root, 0);
+            return;
+        }
+        
+        if(root.left != null && !map.containsKey(root.left))
+            findisSumTree(root.left, map);
+        
+        if(root.right != null && !map.containsKey(root.right))
+            findisSumTree(root.right, map);
+        
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        
+        if(root.left != null){
+            a = map.get(root.left);
+            c = root.left.data;
+        }
+        if(root.right != null){
+            b = map.get(root.right);
+            d = root.right.data;
+        }
+        
+        map.put(root, a+b+c+d);
+	}
+}
