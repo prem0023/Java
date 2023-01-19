6af5ec00a3adea253f3e16da1d1b2e618653988d
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-19 22:29:20 +0530

    Added solution - LeetHub

--- Binary Tree to DLL - GFG/binary-tree-to-dll.java
+++ Binary Tree to DLL - GFG/binary-tree-to-dll.java
@@ -151,34 +151,25 @@ class Solution
 {
     //Function to convert binary tree to doubly linked list and return it.
     Node bToDLL(Node root){
-        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null)
+            return null;
         
-        inOrder(root, list);
+        Node l = bToDLL(root.left);
+        Node r = bToDLL(root.right);
         
-        Node node = new Node(list.get(0));
-        Node prev = null;
-        Node temp = node;
-        int n = list.size();
+        root.right = r;
+        if(r != null)
+            r.left = root;
+        if(l == null)
+            return root;
         
-        for(int i=1; i<n; i++){
-            temp.left = prev;
-            prev = temp;
-            Node t = new Node(list.get(i));
-            temp.right = t;
+        Node temp = l;
+        while(temp.right != null){
             temp = temp.right;
         }
-        temp.left = prev;
+        temp.right = root;
+        root.left = temp;
         
-        return node;
-    }
-    
-    private void inOrder(Node root, ArrayList<Integer> list){
-        if(root == null)
-            return;
-        
-        inOrder(root.left, list);
-        list.add(root.data);
-        inOrder(root.right, list);
-        
+        return l;
     }
 }
