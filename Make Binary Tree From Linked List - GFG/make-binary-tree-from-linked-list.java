c5a55ee989a07cc9b6e6ac7cec7a7c0ba0fbc979
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-20 00:57:01 +0530

    Added solution - LeetHub

---
+++ Make Binary Tree From Linked List - GFG/make-binary-tree-from-linked-list.java
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+import java.util.*;
+class Tree {
+    int data;
+    Tree left;
+    Tree right;
+    Tree(int d) {
+        data = d;
+        left = null;
+        right = null;
+    }
+} 
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class MakeBT {
+    static Node lhead;
+    static Queue<Tree> queue = new LinkedList<Tree>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Tree node = null;
+            lhead = null;
+            Node head = null;
+            while (!queue.isEmpty()) {
+                queue.poll();
+            }
+            int n = sc.nextInt();
+            if (n != 0) {
+                MakeBT llist = new MakeBT();
+                int a1 = sc.nextInt();
+                head = new Node(a1);
+                llist.addToTheLast(head);
+                lhead = head;
+                for (int i = 1; i < n; i++) {
+                    int a = sc.nextInt();
+                    llist.addToTheLast(new Node(a));
+                }
+            }
+            GfG g = new GfG();
+            Tree root = g.convert(lhead, node);
+            try {
+                levelOrder(root, n);
+            } catch (NullPointerException ex) {
+                System.out.println(-1);
+            }
+            System.out.println();
+        }
+    }
+    public static void levelOrder(Tree root, int n) {
+        queue.add(root);
+        while (!queue.isEmpty() && n-- > 0) {
+            Tree proot = queue.remove();
+            try {
+                queue.add(proot.left);
+                queue.add(proot.right);
+                System.out.print(proot.data + " ");
+            } catch (NullPointerException ex) {
+            }
+        }
+    }
+    public void addToTheLast(Node head) {
+        if (lhead == null)
+            lhead = head;
+        else {
+            Node temp = lhead;
+            while (temp.next != null) temp = temp.next;
+            temp.next = head;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Tree{
+int data;
+    Tree left;
+    Tree right;
+    Tree(int d){
+        data=d;
+        left=null;
+        right=null;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+
+class GfG 
+{
+    //Function to make binary tree from linked list.
+    public static Tree convert(Node head, Tree node) {
+        // add code here.}
+        Node temp = head.next;
+        node = new Tree(head.data);
+        Tree tree = node;
+        
+        Queue<Tree> q = new LinkedList<>();
+        q.offer(tree);
+        
+        while(temp != null){
+            tree = q.poll();
+            tree.left = new Tree(temp.data);
+            q.offer(tree.left);
+            temp = temp.next;
+            
+            if(temp != null){
+                tree.right = new Tree(temp.data);
+                q.offer(tree.right);
+            }
+            else
+                break;
+            
+            temp = temp.next;
+        }
+        return node;
+    }
+}  
