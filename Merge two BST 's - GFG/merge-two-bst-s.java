4132a3f6850cf35e3f5bfb7cfc7af44077ac3cd2
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-26 12:30:55 +0530

    Added solution - LeetHub

--- Merge two BST 's - GFG/merge-two-bst-s.java
+++ Merge two BST 's - GFG/merge-two-bst-s.java
@@ -126,43 +126,68 @@ class Solution
     public List<Integer> merge(Node root1,Node root2)
     {
         // Write your code here
-        List<Integer> ans1 = new ArrayList<>();
-        List<Integer> ans2 = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
         
-        inorder(root1, ans1);
-        inorder(root2, ans2);
+        helper(root1, st1);
+        helper(root2, st2);
         
-        mergeList(ans1, ans2, ans);
+        while(!st1.isEmpty() && !st2.isEmpty()){
+            Node temp1 = st1.peek();
+            Node temp2 = st2.peek();
+            
+            if(temp1.data < temp2.data){
+                st1.pop();
+                ans.add(temp1.data);
+                helper(temp1.right, st1);
+            }
+            else{
+                st2.pop();
+                ans.add(temp2.data);
+                helper(temp2.right, st2);
+            }
+        }
+        if(!st1.isEmpty()) {
+        	Node temp = st1.pop();
+        	ans.add(temp.data);
+        	st1.push(temp.right);
+        	inorder(st1, ans);
+        }
+        if(!st2.isEmpty()) {
+        	Node temp = st2.pop();
+        	ans.add(temp.data);
+        	st2.push(temp.right);
+        	inorder(st2, ans);
+        }
         
         return ans;
     }
     
-    private void mergeList(List<Integer> ans1, List<Integer> ans2, List<Integer> ans){
-        int n = ans1.size();
-        int m = ans2.size();
-        
-        int i = 0, j = 0;
-        
-        while(i<n && j<m){
-            if(ans1.get(i) > ans2.get(j))
-                ans.add(ans2.get(j++));
-            else
-                ans.add(ans1.get(i++));
+    private void inorder(Stack<Node> st, List<Integer> ans){
+        if(st.isEmpty())
+            return;
+        Node temp = st.pop();
+        while(temp != null || !st.isEmpty()){
+            while(temp != null){
+                st.push(temp);
+                temp = temp.left;
+            }
+            temp = st.pop();
+            ans.add(temp.data);
+            temp = temp.right;
         }
-        
-        while(i<n)
-            ans.add(ans1.get(i++));
-        while(j<m)
-            ans.add(ans2.get(j++));
     }
     
-    private void inorder(Node root, List<Integer> ans){
+    private void helper(Node root, Stack<Node> st){
         if(root == null)
             return;
         
-        inorder(root.left, ans);
-        ans.add(root.data);
-        inorder(root.right, ans);
+        Node temp = root;
+        
+        while(temp != null){
+            st.push(temp);
+            temp = temp.left;
+        }
     }
 }
