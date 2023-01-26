16a3a8ee3d586beb07dc6776992c01391c613573
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-01-26 12:34:43 +0530

    Update Merge two BST .java

--- Merge two BST .java
+++ Merge two BST .java
@@ -1,6 +1,84 @@
 // Code Link : https://practice.geeksforgeeks.org/problems/merge-two-bst-s/1
 
-// Method - 1
+// Method - 1 (bEST)
+
+class Solution
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        List<Integer> ans = new ArrayList<>();
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
+        
+        helper(root1, st1);
+        helper(root2, st2);
+        
+        while(!st1.isEmpty() && !st2.isEmpty()){
+            Node temp1 = st1.peek();
+            Node temp2 = st2.peek();
+            
+            if(temp1.data < temp2.data){
+                st1.pop();
+                ans.add(temp1.data);
+                helper(temp1.right, st1);
+            }
+            else{
+                st2.pop();
+                ans.add(temp2.data);
+                helper(temp2.right, st2);
+            }
+        }
+        if(!st1.isEmpty()) {
+        	Node temp = st1.pop();
+        	ans.add(temp.data);
+        	st1.push(temp.right);
+        	inorder(st1, ans);
+        }
+        if(!st2.isEmpty()) {
+        	Node temp = st2.pop();
+        	ans.add(temp.data);
+        	st2.push(temp.right);
+        	inorder(st2, ans);
+        }
+        
+        return ans;
+    }
+    
+    private void inorder(Stack<Node> st, List<Integer> ans){
+        if(st.isEmpty())
+            return;
+        Node temp = st.pop();
+        while(temp != null || !st.isEmpty()){
+            while(temp != null){
+                st.push(temp);
+                temp = temp.left;
+            }
+            temp = st.pop();
+            ans.add(temp.data);
+            temp = temp.right;
+        }
+    }
+    
+    private void helper(Node root, Stack<Node> st){
+        if(root == null)
+            return;
+        
+        Node temp = root;
+        
+        while(temp != null){
+            st.push(temp);
+            temp = temp.left;
+        }
+    }
+}
+
+
+
+//mETHOD - 2
+/*
 
 class Solution
 {
@@ -53,9 +131,9 @@ class Solution
         inorder(root.right, ans);
     }
 }
+*/
 
-
-// Method - 2
+// Method - 3
 /*
 class Solution
 {
@@ -107,7 +185,7 @@ class Solution
 */
 
 
-//Method - 3
+//Method - 4
 /*
 class Node  
 { 
