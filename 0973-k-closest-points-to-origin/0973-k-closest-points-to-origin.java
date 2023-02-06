5a0cf721a838f9a01ee860815ad43db04048017c
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-02-07 00:25:48 +0530

    Time: 40 ms (52.52%), Space: 50.7 MB (56.79%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,44 @@
+class Tuple
+{
+    int x, y;
+    double dist;
+    Tuple(int _x, int _y, double _dist){
+        this.x = _x;
+        this.y = _y;
+        this.dist = _dist;
+    }
+}
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] ans = new int[k][2];
+        int n = points.length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b) -> {
+        	if(b.dist > a.dist)
+        		return 1;
+        	else if(b.dist < a.dist)
+        		return -1;
+        	return 0;
+        });
+        
+        int c = 0;
+        for(int i=0; i<n; i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            double dist = Math.sqrt(x*x + y*y);
+            
+            c++;
+            pq.offer(new Tuple(x,y,dist));
+            
+            if(c > k)
+                pq.poll();
+        }
+        c = 0;
+        while(!pq.isEmpty()){
+            ans[c][0] = pq.peek().x;
+            ans[c++][1] = pq.poll().y;
+        }
+        
+        return ans;
+    }
+}
