8fbad9ce85e55ac2576161b5272637e71f8ed6ba
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-02-09 22:25:41 +0530

    Added solution - LeetHub

---
+++ Rearrange characters - GFG/rearrange-characters.java
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int t = Integer.parseInt(br.readLine());
+			while (t-- > 0) {
+				String str = br.readLine();
+				Solution ob= new Solution();
+				String str1 = ob. rearrangeCharacters(str);
+				
+        		int flag=1;
+        	    int[] c = new int[26];
+        	    Arrays.fill(c,0);
+        	    for(int i=0; i<str.length(); i++)
+        	        c[str.charAt(i)-'a']+=1;
+        	    int f = 0;
+        	    int x = (str.length()+1)/2;
+        	    for(int i=0; i<26; i++)
+        	    {
+        	        if(c[i]>x)
+        	            f = 1;
+        	    }
+        	    if(f == 1)
+        	    {
+        	        if(str1=="-1")
+        	            System.out.println(0);
+            	    else
+            	        System.out.println(1);
+        	    }
+        	    else
+        	    {
+            	    int[] a = new int[26];
+        	        Arrays.fill(a,0);
+            	    int[] b = new int[26];
+        	        Arrays.fill(b,0);
+            	    for(int i=0; i<str.length(); i++)
+            	        a[str.charAt(i)-'a']+=1;
+            	    for(int i=0; i<str1.length(); i++)
+            	        b[str1.charAt(i)-'a']+=1;
+            	        
+            	    for(int i=0; i<26; i++)
+            	        if(a[i]!=b[i])
+            	            flag = 0;
+            	    
+            	    for(int i=0;i<str1.length();i++)
+            	    {
+            		    if(i>0)
+            		        if(str1.charAt(i-1)==str1.charAt(i))
+            			        flag=0;
+            	    }
+            	    if(flag==1)
+            		    System.out.println(1);
+            	    else
+            	        System.out.println(0);
+        	    }
+			}
+		} catch (Exception e) {
+
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Pair implements Comparable<Pair>{
+    int count;
+    char c;
+    Pair(char c,int count){
+        this.c=c;
+        this.count=count;
+    }
+    
+    public int compareTo(Pair obj){
+        return obj.count-this.count;
+    }
+}
+
+class Solution
+{
+	public static String rearrangeCharacters(String S) {
+	
+		int[] hash = new int[26];
+        int max = 0, letter = 0;
+        for (int i = 0; i < S.length(); i++) {
+            hash[S.charAt(i) - 'a']++;
+        } 
+        
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
+            }
+        }
+        if (max > (S.length() + 1) / 2) {
+            return "-1"; 
+        }
+        char[] res = new char[S.length()];
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            hash[letter]--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i] > 0) {
+                if (idx >= res.length) {
+                    idx = 1;
+                }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                hash[i]--;
+            }
+        }
+        return String.valueOf(res);
+		
+	}
+}
