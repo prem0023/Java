59f02b57d6848e9c1760fd8a87392749ed5fb58f
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-03-10 20:08:57 +0530

    Time: 14 ms (23.36%), Space: 44.9 MB (14.89%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -1,37 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     private ListNode head;
+    private Random random;
 
     public Solution(ListNode head) {
         this.head = head;
+        random = new Random();
     }
-    
+
     public int getRandom() {
-        int scope = 1, chosenValue = 0;
-        ListNode curr = this.head;
+        int count = 0;
+        int result = 0;
+        ListNode curr = head;
+
         while (curr != null) {
-            // decide whether to include the element in reservoir
-            if (Math.random() < 1.0 / scope)
-                chosenValue = curr.val;
-            // move on to the next node
-            scope += 1;
+            count++;
+            // generate a random number between 1 and the count
+            // if the random number is 1, update the result with the current node's value
+            if (random.nextInt(count) == 0) {
+                result = curr.val;
+            }
             curr = curr.next;
         }
-        return chosenValue;
+
+        return result;
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(head);
- * int param_1 = obj.getRandom();
- */
