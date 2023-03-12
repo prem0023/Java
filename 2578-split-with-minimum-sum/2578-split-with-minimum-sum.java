7c61c0e607fb4b28def780d76bf07ae91b87481a
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-03-12 21:52:35 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (75.54%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
@@ -1,5 +1,48 @@
 class Solution {
     public int splitNum(int num) {
+        int n = 0;
+        int or = num;
+        while(num > 0){
+            n++;
+            num = num / 10;
+        }
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++){
+            a[i] = or % 10;
+            or = or / 10;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - 1; j++){
+                if(a[j + 1] < a[j]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+
+            }
+    }
+
+        int num1 = 0, num2 = 0;
+        for(int i = 0; i < n - 1; i+=2){
+            num1 = num1 * 10 + a[i];
+            num2 = num2 * 10 + a[i + 1];
+        }
+        if(n % 2 != 0){
+            num1 = num1 * 10 + a[n-1];
+        }
+
+        return num1 + num2;
+
+
+        
+    }
+}
+
+/*
+class Solution {
+    public int splitNum(int num) {
         ArrayList<Integer> list = new ArrayList<>();
         int n = num;
         
@@ -23,7 +66,7 @@ class Solution {
                 flag = true;
             }
         }
-        // System.out.println("num1 : " + num1 + " num2 : " + num2);
         return num1 + num2;
     }
 }
+*/
