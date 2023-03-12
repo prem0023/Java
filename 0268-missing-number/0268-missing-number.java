b3d60a54408f7964a7b982ed4d5dbc4118a0399f
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-03-12 22:31:36 +0530

    Time: 11 ms (5.82%), Space: 43.3 MB (39.86%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int except = -1;
+        
+        for(int i=0; i<n; i++){
+            int x = Math.abs(nums[i]);
+            if(x >= n)
+                continue;
+            nums[x] = -1 * nums[x];
+            if(nums[x] == 0)
+                except = x;
+        }
+        System.out.println(Arrays.toString(nums));
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] >= 0 && i != except)
+                return i;
+        }
+        
+        return n;
+    }
+}
