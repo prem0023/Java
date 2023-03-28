3a54b565548172bdf8d55adf317d2cab0ead7d3b
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-03-29 00:51:13 +0530

    Time: 3 ms (19.65%), Space: 43.9 MB (40.00%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int indexZero = 0, indexNonZero = 1;
+        int n = nums.length;
+        
+        // 0,1,0,3,12
+        while(indexNonZero < n){
+            if(nums[indexZero] == 0){
+                while(indexNonZero < n && nums[indexNonZero] == 0)
+                    indexNonZero++;
+                
+                if(indexNonZero == n)
+                    return;
+                
+                int temp = nums[indexNonZero];
+                nums[indexNonZero] = nums[indexZero];
+                nums[indexZero] = temp;
+                indexNonZero++;
+            }
+            indexZero++;
+            indexNonZero = Math.max(indexNonZero, indexZero+1);
+        }
+    }
+}
