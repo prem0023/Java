77889ebd7e8a924f767cd3a7e522c12a7c49227e
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-03-30 22:01:04 +0530

    Time: 2 ms (43.52%), Space: 43.8 MB (51.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,41 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // int indexZero = 0, indexNonZero = 1;
         int n = nums.length;
+        int zeroCount = 0;
         
         for(int i=0; i<n; i++){
-            if(nums[i] != 0)
-                continue;
-            for(int j=i+1; j<n; j++){
-                if(nums[j] != 0){
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                    break;
-                }
+            if(nums[i] == 0)
+                zeroCount++;
+            else if(zeroCount > 0){
+                nums[i-zeroCount] = nums[i];
+                nums[i] = 0;
             }
         }
     }
 }
+
+/*
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int indexZero = 0, indexNonZero = 1;
+        int n = nums.length;
+        
+        // 0,1,0,3,12
+        while(indexNonZero < n){
+            if(nums[indexZero] == 0){
+                while(indexNonZero < n && nums[indexNonZero] == 0)
+                    indexNonZero++;
+                
+                if(indexNonZero == n)
+                    return;
+                
+                nums[indexZero] = nums[indexNonZero];
+                nums[indexNonZero] = 0;
+                indexNonZero++;
+            }
+            indexZero++;
+            indexNonZero = Math.max(indexNonZero, indexZero+1);
+        }
+    }
+}
+*/
