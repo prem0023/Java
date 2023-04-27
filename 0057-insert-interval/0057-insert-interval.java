889083db603aa37765d22f529bc00a934476fc03
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-04-28 00:13:56 +0530

    Time: 1 ms (97.73%), Space: 45.2 MB (6.88%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,15 +1,44 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        int i = 0, n = intervals.length;
-        while (i < n && intervals[i][1] < newInterval[0]) 
-            res.add(intervals[i++]);
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval = new int[] {Math.min(intervals[i][0], newInterval[0]), Math.max(intervals[i][1], newInterval[1])}; 
-            i++;
+        ArrayList<int[]> list = new ArrayList<>();
+        int n = intervals.length;
+        
+        for(int i=0; i<n; i++){
+            if(newInterval[1] < intervals[i][0]){
+                list.add(newInterval);
+                int[][] ans = new int[list.size() + n-i][2];
+                int k=0;
+                for(int[] x : list){
+                    ans[k][0] = x[0];
+                    ans[k++][1] = x[1];
+                }
+                // System.out.println(ans.length+ " " + k);
+                while(i < n){
+                    ans[k][0] = intervals[i][0];
+                    ans[k++][1] = intervals[i][1];
+                    i++;
+                }
+                
+                return ans;
+            }
+            else if(newInterval[0] > intervals[i][1])
+                list.add(intervals[i]);
+            else{
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
         }
-        res.add(newInterval);
-        while (i < n) res.add(intervals[i++]);
-        return res.toArray(new int[res.size()][2]);
+        int k=0;
+        int[][] ans = new int[list.size() + 1][2];
+        
+        for(int[] x : list){
+            ans[k][0] = x[0];
+            ans[k++][1] = x[1];
+        }
+        
+        ans[k][0] = newInterval[0];
+        ans[k++][1] = newInterval[1];
+        
+        return ans;
     }
 }
