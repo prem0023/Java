17b2e79e4fc8ca69ad53237c50f919172b046114
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-05-10 23:48:18 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (17.46%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,47 +1,29 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int ans[][]=new int[n][n];
-
-        int l=0,r=n-1; //col
-        int t=0,b=n-1;//row
-
-        int num=1;
-        int dir=0; //direction
-
-        while(l<=r && t<=b){
-            if(dir==0){
-                for(int i=l;i<=r;i++){
-                    ans[t][i]=num;
-                    num++;
-                }
-                dir=1;
-                t++; // increased top row
-            }
-            else if(dir==1){
-                for(int i=t;i<=b;i++){
-                    ans[i][r]=num;
-                    num++;
-                }
-                dir=2;
-                r--; // decrease right col
-            }
-            else if(dir==2){
-                for(int i=r;i>=l;i--){
-                    ans[b][i]=num;
-                    num++;
-                }
-                dir=3;
-                b--; // decrease bottum row
-            }
-            else if(dir==3){
-                for(int i=b;i>=t;i--){
-                    ans[i][l]=num;
-                    num++;
-                }
-                dir=0;
-                l++; //increase left col
-            }
+        int [][] a = new int[n][n];
+        int l=0,t=0,b=n-1,r=n-1,v=1;
+        while(t<=b||l<=r){
+         if(t<=b){
+             for(int i=l;i<=r;i++)
+                a[t][i]=v++;
+             t++;
+         }
+         if(l<=r){
+             for(int i=t;i<=b;i++)
+                 a[i][r]=v++;
+             r--;
+         }
+         if(t<=b){
+             for(int i=r;i>=l;i--)
+             a[b][i]=v++;
+             b--;
+         }
+         if(t<=b){
+             for(int i=b;i>=t;i--)
+             a[i][l]=v++;
+         }
+         l++;
         }
-        return ans;
+        return a;
     }
 }
