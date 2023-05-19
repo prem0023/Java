e4f87dc420147604075c6114d7b858ca37232c0d
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-05-19 23:07:38 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (11.65%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,25 +1,24 @@
 class Solution {
-    private boolean isSameColored(int node,int color,int[] vis,int[][] graph){
-        vis[node]=color;
-        //neighbours
-        for(int it:graph[node]){
-            if(vis[it]==0){
-                if(isSameColored(it,-color,vis,graph)==true)
-                    return true;
-            }
-            else if(vis[it]==color)
-                return true;
-        }
-        return false;
-        
-    }
     public boolean isBipartite(int[][] graph) {
-        int n=graph.length;
-        int[] vis = new int[n];
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                if(isSameColored(i,1,vis,graph)==true)
-                    return false;
+        int n = graph.length;
+        int[] colors = new int[n];			
+				
+        for (int i = 0; i < n; i++) {              //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }       
+        colors[node] = color;       
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
             }
         }
         return true;
