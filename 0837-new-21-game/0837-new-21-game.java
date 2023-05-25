9cef81e7b2c06bc639d258ffb3d8755223e92810
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-05-25 22:48:57 +0530

    Time: 3 ms (98.80%), Space: 43 MB (7.23%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if(k==0 || n>=k+maxPts-1) return 1.0;
+        double dp[]=new double[n+maxPts];
+        double prob=(double)1/(double)maxPts;
+        double sum=0.0;
+        int j=k-1+maxPts;
+        for(int i=n;i>=0;i--) {
+            if(i>=k) {
+                dp[i]=1.0;
+            }
+            else {
+                dp[i]+=prob*sum;
+                if(j-i==maxPts) {
+                    sum-=dp[j--];
+                }
+            }
+            sum+=dp[i];
+        }
+        return dp[0];
+    }
+}
