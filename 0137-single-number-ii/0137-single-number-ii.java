b47b56fce717d575a70e4d66c33f510e5aae30c2
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-07-04 23:20:38 +0530

    Time: 5 ms (42.53%), Space: 44.1 MB (39.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,18 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i = 0; i < 32; i++) {
-            int sum = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(((nums[j] >> i) & 1) == 1) {
-                    sum++;
-                    sum %= 3;
-                }
-            }
-            if(sum != 0) {
-                ans |= sum << i;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
             }
         }
-        return ans;
+        
+        return -1;
     }
 }
