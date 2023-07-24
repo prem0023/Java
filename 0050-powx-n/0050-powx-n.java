35f5bdb0e0c7c3918784963044c380d9c883904d
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-07-24 22:37:20 +0530

    Time: 1 ms (16.69%), Space: 41.6 MB (5.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,10 +1,25 @@
-class Solution {
-    public double myPow(double x, int n) {
-        if(n == 0)
-            return 1;
-        if(n<0){
-            return 1/x * myPow(1/x, -(n + 1));
+public class Solution {
+    public double solve(double x, long n) {
+        if (n == 0) {
+            return 1; // power of 0 is 1
         }
-        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+        double temp = solve(x, n / 2);
+        temp = temp * temp;
+
+        if (n % 2 == 0) { // if even, return just without doing anything
+            return temp;
+        } else {
+            return temp * x; // if odd, return by multiplying once more with the given number
+        }
+    }
+
+    public double myPow(double x, int n) {
+        long longN = n; // Convert n to a long to handle Integer.MIN_VALUE
+        double ans = solve(x, Math.abs(longN));
+
+        if (longN < 0) {
+            return 1 / ans;
+        }
+        return ans;
     }
 }
