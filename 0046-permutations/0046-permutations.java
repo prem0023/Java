61b175998d9adc255f046046d871f802f8d62d6e
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-08-02 22:42:06 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (31.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,29 @@
-class Solution {
-    public List<List<Integer>> permute(int[] num) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        if (num.length ==0) return ans;
-        List<Integer> l0 = new ArrayList<Integer>();
-        l0.add(num[0]);
-        ans.add(l0);
-        for (int i = 1; i< num.length; ++i){
-            List<List<Integer>> new_ans = new ArrayList<List<Integer>>(); 
-            for (int j = 0; j<=i; ++j){            
-               for (List<Integer> l : ans){
-                   List<Integer> new_l = new ArrayList<Integer>(l);
-                   new_l.add(j,num[i]);
-                   new_ans.add(new_l);
-               }
-            }
-            ans = new_ans;
+public class Solution {
+    public void permuteRec(int[] nums, int begin, List<List<Integer>> result) {
+        if (begin == nums.length) {
+            List<Integer> temp = new ArrayList<Integer>();
+            for (int num : nums) temp.add(num);
+            result.add(temp);
+            return;
         }
-        return ans;
+        for (int i = begin; i < nums.length; i++) {
+            // Swap
+            int temp = nums[begin];
+            nums[begin] = nums[i];
+            nums[i] = temp;
+            
+            permuteRec(nums, begin + 1, result);
+            
+            // Swap back
+            temp = nums[begin];
+            nums[begin] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        permuteRec(nums, 0, result);
+        return result;
     }
 }
