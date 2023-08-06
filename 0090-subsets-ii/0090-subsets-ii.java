6ad2c825503500541446897c5240a0685fc9ba5e
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-08-06 19:28:02 +0530

    Time: 2 ms (34.68%), Space: 43.5 MB (94.05%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,40 @@
 class Solution {
-    static void subsequence(int i,int[] arr, List<Integer> lst, List<List<Integer>> res){
-        if(res.contains(lst)) return;
-        if(i==arr.length){
-            res.add(new ArrayList<>(lst));
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
+        List<Integer> temp = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        helper(nums, set, temp, 0, nums.length, true);
+        
+        List<List<Integer>> ans  = new ArrayList<>(set);
+        
+        return ans;
+    }
+    
+    private void helper(int[] nums, HashSet<List<Integer>> set, List<Integer> temp, int i, int n, boolean flag){
+        if(i == n){
+            if(flag)
+                set.add(new ArrayList<>(temp));
+            
             return;
         }
-        lst.add(arr[i]);
-        subsequence(i+1, arr, lst, res);
-        lst.remove(lst.size()-1);
-        subsequence(i+1, arr, lst, res);
-    }
-
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res=new ArrayList<>();
-        subsequence(0, nums, new ArrayList<>(), res);
-        return res;
+        
+        if(flag)
+            set.add(new ArrayList<>(temp));
+        
+        temp.add(nums[i]);
+        helper(nums, set, temp, i+1, nums.length, true);
+        temp.remove(temp.size()-1);
+        
+        int x = nums[i];
+        while(i<n && nums[i] == x)
+            i++;
+        
+        if(i == n)
+            return;
+        
+        i--;
+        
+        helper(nums, set, temp, i+1, nums.length, false);
     }
 }
