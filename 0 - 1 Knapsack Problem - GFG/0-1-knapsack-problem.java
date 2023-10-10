21374270d4f948067e187d98fc2086b7f9f1393f
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-10-11 01:02:47 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -48,22 +48,40 @@ class gfg
 
 class Solution 
 { 
+    //Function to return max value that can be put in knapsack of capacity W.
+    
     static int knapSack(int W, int wt[], int val[], int n) 
     {
-        int[][] dp = new int[n+1][W+1];
+        int[][] dp = new int[W+1][n+1];
         
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=W; j++){
-                if(wt[i-1] <= j){
-                    dp[i][j] = Math.max(dp[i-1][j], val[i-1] + dp[i-1][j-wt[i-1]]);
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+        for(int i=0; i<=W; i++){
+            for(int j=0; j<=n; j++){
+                dp[i][j] = -1;
             }
         }
+        return helper(W, wt, val, n, dp);
+    } 
+    
+    private static int helper(int W, int wt[], int val[], int n, int[][] dp){
+        if(n == 0 || W == 0)
+            return 0;
         
-        return dp[n][W];
+        if(dp[W][n] != -1)
+            return dp[W][n];
+        
+        if(W >= wt[n-1]){
+            int include = val[n-1] + helper(W-wt[n-1], wt, val, n-1, dp);
+            int exclude = helper(W, wt, val, n-1, dp);
+            
+            dp[W][n] = Math.max(include, exclude);
+            
+            return dp[W][n];
+        }
+        
+        int exclude = helper(W, wt, val, n-1, dp);
+        dp[W][n] = exclude;
+        
+        return dp[W][n];
     }
 }
 
