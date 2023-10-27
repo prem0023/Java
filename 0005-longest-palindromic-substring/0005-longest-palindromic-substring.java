90adfc8b0277820b2ebf873cb4acc9ba5d23e971
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-10-27 23:14:51 +0530

    Time: 573 ms (9.15%), Space: 43.9 MB (43.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,56 +1,36 @@
 public class Solution {
-    private int lo, maxLen;
-
     public String longestPalindrome(String s) {
-        int len = s.length();
-        if (len < 2)
+        if (s.length() <= 1) {
             return s;
-
-        for (int i = 0; i < len-1; i++) {
-            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-            extendPalindrome(s, i, i+1); //assume even length.
-        }
-        return s.substring(lo, lo + maxLen);
-    }
-
-    private void extendPalindrome(String s, int j, int k) {
-        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
-        }
-        if (maxLen < k - j - 1) {
-            lo = j + 1;
-            maxLen = k - j - 1;
         }
 
-    }
-}
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
 
-/*
-class Solution {
-     public String longestPalindrome(String s) {
-        int len = s.length();
-        if (len <= 1) return s;
-        boolean[][] dp = new boolean[len][len];
-        int ml = 0;
-        int mr = 0;
-        for (int r = 0; r < len; r++) {
-            dp[r][r] = true;
-            for (int l = 0; l < r; l++) {
-                // l  r
-                // abca ...  //a = a, ok
-                if (s.charAt(l) != s.charAt(r)) continue;
-                // l  r
-                // abca ...   //b != c continue
-                if (l + 1 < r - 1 && !dp[l + 1][r - 1]) continue;
-                dp[l][r] = true;
-                if (r - l > mr - ml) {
-                    ml = l;
-                    mr = r;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
                 }
             }
         }
-        return s.substring(ml, mr + 1);
+
+        return maxStr;
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
-*/
