dd85ddb8a9591165aed2c04d307c5979d760b41b
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2023-12-30 13:20:05 +0530

    Time: 4 ms (43.02%), Space: 42.6 MB (67.92%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean makeEqual(String[] words) {
         int[] ch = new int[26];
+        int n = words.length;
         
-        for(int i=0; i<words.length; i++){
+        for(int i=0; i<n; i++){
             for(int j=0; j<words[i].length(); j++)
                 ch[words[i].charAt(j) - 'a']++;
         }
         
+        // System.out.println(Arrays.toString(ch));
+        
         for(int i=0; i<26; i++){
-            if(ch[i] % words.length != 0)
+            if(ch[i] % n != 0)
                 return false;
         }
         return true;
