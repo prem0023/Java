ab8328030f779e7e6e1d7e06628a5a1accbf0d2b
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2024-04-18 00:18:01 +0530

    Time: 1 ms (99.37%), Space: 44.2 MB (71.49%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String smallestFromLeaf(TreeNode root) {
+        StringBuilder smallest = new StringBuilder();
+        dfs(root, new StringBuilder(), smallest);
+        return smallest.toString();
+    }
+    
+    private void dfs(TreeNode node, StringBuilder path, StringBuilder smallest) {
+        if (node == null) return;
+        
+        // Append current node's character to the path
+        path.append((char)('a' + node.val));
+        
+        // If it's a leaf node, compare and update smallest
+        if (node.left == null && node.right == null) {
+            String currentString = path.reverse().toString();
+            if (smallest.length() == 0 || currentString.compareTo(smallest.toString()) < 0) {
+                smallest.setLength(0);
+                smallest.append(currentString);
+            }
+            path.reverse(); // backtrack by reversing again
+        }
+        
+        // Recursively traverse left and right subtrees
+        dfs(node.left, path, smallest);
+        dfs(node.right, path, smallest);
+        
+        // Backtrack: remove the current node's character from the path
+        path.setLength(path.length() - 1);
+    }
+}
