53d57c9b9d9e7fba2bd74761195d03fd439f13bb
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2024-09-05 22:53:47 +0530

    Time: 6 ms (23.61%), Space: 59.7 MB (81.64%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.java
+++ 2028-find-missing-observations/2028-find-missing-observations.java
@@ -13,27 +13,16 @@ class Solution {
             return new int[0];
         
         int[] missingRolls = new int[n];
-        int remainingSum = totalSum-sum; // 21 - 12 = 9
-        // System.out.println("remainingSum: " + remainingSum);
+        int remainingSum = totalSum-sum;
         
         for(int i=n; i>0; i--){
-            int num = random.nextInt(6) + 1;
-            // System.out.println("num: " + num);
-            
-            if((i-1)*6 >= remainingSum-num && (remainingSum-num) >= i-1 ){
-                missingRolls[i-1] = num;
-                remainingSum -= num;
-            }
-            else{
-                for(int x=1; x<=6; x++){
-                    if((remainingSum-x) <= (i-1)*6){
-                        missingRolls[i-1] = x;
-                        remainingSum -= x;
-                        break;
-                    }
+            for(int x=1; x<=6; x++){
+                if((remainingSum-x) <= (i-1)*6){
+                    missingRolls[i-1] = x;
+                    remainingSum -= x;
+                    break;
                 }
             }
-                
         }
         return missingRolls;
     }
