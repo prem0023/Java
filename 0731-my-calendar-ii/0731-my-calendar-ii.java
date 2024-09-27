fd7c83e702576424b3a2cd154bb5ee7baed5d1c3
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2024-09-28 00:57:36 +0530

    Time: 302 ms (12.54%), Space: 46 MB (27.40%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -0,0 +1,30 @@
+class MyCalendarTwo {
+    private TreeMap<Integer, Integer> pointFreq = null;
+    public MyCalendarTwo() {
+          pointFreq = new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        pointFreq.put(start, pointFreq.getOrDefault(start, 0) + 1);
+        pointFreq.put(end, pointFreq.getOrDefault(end, 0) - 1);
+        if(causingTripleBooking(start, end))return false;
+        return true;
+    }
+    private boolean causingTripleBooking(int start, int end){
+        int overlapCnt = 0;
+        for(var entry : pointFreq.entrySet()){
+            overlapCnt += entry.getValue();
+            if(overlapCnt > 2){
+                pointFreq.put(start, pointFreq.getOrDefault(start, 0) - 1);
+                pointFreq.put(end, pointFreq.getOrDefault(end, 0) + 1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
