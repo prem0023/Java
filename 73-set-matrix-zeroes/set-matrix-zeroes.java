c4bee148c298fffd3587b2bdcba3c741599168dd
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2026-03-29 16:39:11 +0530

    Time: 1 ms (72.91%) | Memory: 47.7 MB (11.22%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,27 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> row = new LinkedHashSet<>();
-        Set<Integer> column = new LinkedHashSet<>();
+        int[] row = new int[matrix.length];
+        int[] column = new int[matrix[0].length];
 
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    row.add(i);
-                    column.add(j);
+                    row[i] = -1;
+                    column[j] = -1;
                 }
             }
         }
 
-        for(int r : row){
+        for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                matrix[r][j] = 0;
+                if(row[i] == -1 || column[j] == -1){
+                    matrix[i][j] = 0;
+                }
             }
         }
 
-        for(int c : column){
-            for(int i=0; i<matrix.length; i++){
-                matrix[i][c] = 0;
-            }
-        }
     }
 }
