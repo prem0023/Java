662a78a32870ec69261b2dfa37b7b2e36e0ee393
Phanishwar Nath <47606671+prem0023@users.noreply.github.com>
2026-04-15 01:28:09 +0530

    Time: 1 ms (95.54%) | Memory: 43.3 MB (75.48%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,10 +1,8 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalTriangle = new ArrayList<>();
-        pascalTriangle.add(new ArrayList<>());
-        pascalTriangle.get(0).add(1);
 
-        for(int i=1; i<numRows; i++){
+        for(int i=0; i<numRows; i++){
             List<Integer> list = new ArrayList<>();
             for(int j=0; j<=i; j++){
                 if(j == 0 || j == i){
